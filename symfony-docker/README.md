5609084080ca00c364a8eb9135d856d37708bb13
Jordan-Philippot <jordan.philippot@epitech.eu>
2020-12-12 17:50:12 +0100

    project Ok

---
+++ symfony-docker/README.md
@@ -0,0 +1,11 @@
+## START SERVER SQL
+
+##### docker-compose exec db mysql -usymfony_user -psymfony_password
+
+## ON CONTAINER CHANGE
+
+##### docker-compose up -d
+
+## START CONTAINER PHP
+
+##### docker exec -it symfony-docker_php_1 bash
---
+++ symfony-docker/symfony/src/Entity/User.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
